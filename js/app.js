19ca4d001826031d59516d946cdb21a299f14788
dhela19 <ubongabasijerome8@gmail.com>
2023-04-28 23:19:20 +0100

    Image dimension fix for desktop

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-fetchJoke = () =>{
-    sessionStorage.setItem("count" , "clicked")
+fetchJoke = () => {
+    sessionStorage.setItem("count", "clicked")
     try {
         console.log("fetching json data...")
         document.getElementById("jokesDisplay").innerText = "Loading..."
         fetch(`https://api.chucknorris.io/jokes/random`)
-        // fetch(`../res/demo.json`)
+            // fetch(`../res/demo.json`)
             .then((res) => res.json())
             .then((data) => {
                 // do stuff with the data
@@ -17,10 +17,16 @@ fetchJoke = () =>{
     }
 }
 
-jokes = (data) =>{
+jokes = (data) => {
     document.getElementById("jokesDisplay").innerText = data.value
     document.getElementById("avatar").src = "../res/cn.jpg"
-    if(sessionStorage.getItem("count").length > 0){
+    document.getElementById("avatar").style.width = "30%"
+    document.getElementById("avatar").style.height = "50%"
+    if (window.innerWidth < 500) {
+        document.getElementById("avatar").style.width = "50%"
+        document.getElementById("avatar").style.height = "30%"
+    }
+    if (sessionStorage.getItem("count").length > 0) {
         document.getElementById("generateBtn").innerHTML = "AGAIN!"
         document.getElementById("memeBtn").innerHTML = "MEME"
     }
@@ -29,13 +35,13 @@ jokes = (data) =>{
 
 
 
-fetchMeme = () =>{
-    sessionStorage.setItem("count" , "clicked")
+fetchMeme = () => {
+    sessionStorage.setItem("count", "clicked")
     try {
         console.log("fetching json data...")
         document.getElementById("jokesDisplay").innerText = "Loading..."
         fetch(`https://meme-api.com/gimme`)
-        // fetch(`../res/demo.json`)
+            // fetch(`../res/demo.json`)
             .then((res) => res.json())
             .then((data) => {
                 // do stuff with the data
@@ -48,14 +54,14 @@ fetchMeme = () =>{
     }
 }
 
-meme = (data) =>{
+meme = (data) => {
     document.getElementById("jokesDisplay").innerText = data.title;
     document.getElementById("avatar").src = data.url;
-    if(window.innerWidth < 500){
+    if (window.innerWidth < 500) {
         document.getElementById("avatar").style.width = "80%"
         document.getElementById("avatar").style.height = "50%"
-    }    
-    if(sessionStorage.getItem("count").length > 0){
+    }
+    if (sessionStorage.getItem("count").length > 0) {
         document.getElementById("memeBtn").innerHTML = "AGAIN!"
         document.getElementById("generateBtn").innerHTML = "CHUCK NORRIS"
 
