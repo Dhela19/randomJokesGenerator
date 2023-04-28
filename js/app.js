a7e650d8c6e7561524e82a5b80ca60d61b7fb53e
dhela19 <ubongabasijerome8@gmail.com>
2023-04-28 22:25:12 +0100

    added memes

--- js/app.js
+++ js/app.js
@@ -9,18 +9,53 @@ fetchJoke = () =>{
             .then((data) => {
                 // do stuff with the data
                 resultFromJson = data;
-                process(data)
+                jokes(data)
             });
     } catch (err) {
         console.log(err)
         document.getElementById("jokesDisplay").innerText = "Chuck Norris killed the internet again"
     }
 }
-process = (data) =>{
+
+jokes = (data) =>{
     document.getElementById("jokesDisplay").innerText = data.value
     document.getElementById("avatar").src = "../res/cn.jpg"
     if(sessionStorage.getItem("count").length > 0){
         document.getElementById("generateBtn").innerHTML = "AGAIN!"
+        document.getElementById("memeBtn").innerHTML = "MEME"
+    }
+    document.getElementById("avatar").style.display = "flex"
+}
+
+
+
+fetchMeme = () =>{
+    sessionStorage.setItem("count" , "clicked")
+    try {
+        console.log("fetching json data...")
+        document.getElementById("jokesDisplay").innerText = "Loading..."
+        fetch(`https://meme-api.com/gimme`)
+        // fetch(`../res/demo.json`)
+            .then((res) => res.json())
+            .then((data) => {
+                // do stuff with the data
+                resultFromJson = data;
+                meme(data)
+            });
+    } catch (err) {
+        console.log(err)
+        document.getElementById("jokesDisplay").innerText = "Chuck Norris killed the internet again"
+    }
+}
+
+meme = (data) =>{
+    console.log(data)
+    document.getElementById("jokesDisplay").innerText = data.title;
+    document.getElementById("avatar").src = data.url;
+    if(sessionStorage.getItem("count").length > 0){
+        document.getElementById("memeBtn").innerHTML = "AGAIN!"
+        document.getElementById("generateBtn").innerHTML = "CHUCK NORRIS"
+
     }
     document.getElementById("avatar").style.display = "flex"
 }
